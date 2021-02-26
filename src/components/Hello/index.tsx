db1221c5ccb343dda8e6f93da414abd4b43f48b7
Paul Maillardet <paul@pulsanova.com>
2021-02-26 17:22:10 +0100

    Supprime du code inutile

--- src/components/Hello/index.tsx
+++
@@ -1,9 +0,0 @@
-import './index.scss';
-
-const Hello = () => (
-    <h1 className="Hello">
-        Hello World!
-    </h1>
-);
-
-export default Hello;
--- src/components/Logo/index.tsx
+++
@@ -1,7 +0,0 @@
-import LogoImage from './logo.svg';
-
-const Logo = () => (
-    <img className="Logo" src={LogoImage} />
-);
-
-export default Logo;
